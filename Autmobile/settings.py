e454bfbe00547e31a44c3eabb68f289fa570b6d4
saipcr <cr.saiprasad@gmail.com>
2024-09-12 08:02:57 +0000

    Commit

--- Autmobile/settings.py
+++ Autmobile/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/templates/"
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'templates')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
